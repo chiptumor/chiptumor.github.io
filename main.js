783fd8bedfcc385de891a605a4e71912d9608f16
chiptumor <141371172+chiptumor@users.noreply.github.com>
2025-10-07 14:49:25 -0500

    add content files

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ await main.addClickToCopy();
 import * as Lucide from "https://unpkg.com/lucide@latest";
 // TODO: check if namespace or default import
 window.addEventListener("load", Lucide.createIcons);
+
+// ANNOUNCEMENT
+window.addEventListener("load", async () => {
+	// https://api.github.com/repos/chiptumor/chiptumor.github.io/commits?path=content/announcement.xml&page=1&per_page=1
+});
+
+// STATUS
+window.addEventListener("load", async () => {
+	// https://api.github.com/repos/chiptumor/chiptumor.github.io/commits?path=content/status.xml&page=1&per_page=1
+});
