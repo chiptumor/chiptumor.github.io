62e335a41818d0ac745df2c0463dd264f00447ad
betterboruu <nightofthebuttlickers@gmail.com>
2025-10-11 18:55:29 -0500

    fix announcement and status loading

--- main.js
+++ main.js
@@ -20,10 +20,25 @@ window.addEventListener("load", async () => {
 		= content.getElementsByTagName("preview")[0].innerHTML;
 	document.querySelector("[data-content='announcement/body/content']").innerHTML
 		= content.getElementsByTagName("body")[0].innerHTML;
+	
 
+	// DISPLAY DATE
 	const [{ commit: { author: { date: dateString }}}] = await fetch(
 		"https://api.github.com/repos/chiptumor/chiptumor.github.io/commits?path=content/announcement.xml&page=1&per_page=1"
 	).then(response => response.json());
+
+	const date = new Date(dateString);
+	
+	const timeAgo = new TimeAgo("en-US");
+
+	const relative = timeAgo.format(date.getTime());
+	const locale = date.toLocaleString();
+
+	document.querySelector("[data-content='announcement/preview/date']").textContent
+		= relative;
+	document.querySelector("[data-content='announcement/body/date']").textContent
+		= locale;
+
 
 	// TOGGLE OPEN/CLOSE
 	const banner = document.querySelector("#grid div.announcement");
@@ -63,5 +78,30 @@ window.addEventListener("load", () => {
 
 // STATUS
 window.addEventListener("load", async () => {
-	// https://api.github.com/repos/chiptumor/chiptumor.github.io/commits?path=content/status.xml&page=1&per_page=1
+	// DISPLAY CONTENT
+	const content = await fetch("/content/status.xml", { cache: "no-cache" })
+		.then(response => response.text())
+		.then(text => new DOMParser().parseFromString(text, "text/xml"));
+
+	document.querySelector("[data-content='status/imFeeling']").innerHTML
+		= content.getElementsByTagName("imFeeling")[0].innerHTML;
+	document.querySelector("[data-content='status/body']").innerHTML
+		= content.getElementsByTagName("body")[0].innerHTML;
+	
+	// DISPLAY DATE
+	const [{ commit: { author: { date: dateString }}}] = await fetch(
+		"https://api.github.com/repos/chiptumor/chiptumor.github.io/commits?path=content/status.xml&page=1&per_page=1"
+	).then(response => response.json());
+
+	const date = new Date(dateString);
+	
+	const timeAgo = new TimeAgo("en-US");
+
+	const relative = timeAgo.format(date.getTime());
+	const locale = date.toLocaleString();
+
+	document.querySelector("[data-content='status/date/relative']").textContent
+		= relative;
+	document.querySelector("[data-content='status/date/locale']").textContent
+		= locale;
 });
