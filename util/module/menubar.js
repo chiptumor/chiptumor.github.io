e1b55132a116602e3073529de17ea4d1e7fff530
chiptumor <141371172+chiptumor@users.noreply.github.com>
2025-10-13 14:49:23 -0500

    begin menubar.js

--- util/module/menubar.js
+++ util/module/menubar.js
@@ -1,3 +1,19 @@
+function dropdown(node) {
+
+};
+
 export async function addMenubar() {
-	console.info("ran addMenubar");
+	console.time("Create Menubar");
+	const menubar = await fetch("/util/module/resource/menubar.xml")
+		.then(response => response.text())
+		.then(text => new DOMParser().parseFromString(text, "text/xml"));
+
+	const element = document.createElement("div");
+	element.setAttribute("id", "menubar");
+
+	for (const node of menubar.documentElement.children) {
+		if (node.tagName === "dropdown") {}
+	}
+
+	console.timeEnd("Create Menubar");
 };
