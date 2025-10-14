d84c199a360298be17db21f3fa76f7446899f053
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2025-10-14 00:51:06 -0500

    set click to copy

--- util/module/copy.js
+++ util/module/copy.js
@@ -1,3 +1,24 @@
 export function setClickToCopy() {
-	console.info("ran setClickToCopy");
+	console.time("Set Click-to-Copy");
+
+	window.addEventListener("click", async (event) => {
+		const element = event.target.closest("[data-copy]");
+		if (element) {
+			const title = document.getElementById("title");
+
+			try {
+				navigator.clipboard.writeText(element.getAttribute("data-copy"));
+				title.classList.add("ctc-success");
+			} catch(error) {
+				console.error("Unable to copy text:", error);
+				title.classList.add("ctc-failure");
+			}
+
+			setTimeout(() => {
+				title.classList.remove("ctc-success", "ctc-failure");
+			}, 1500);
+		}
+	});
+
+	console.timeEnd("Set Click-to-Copy");
 };
