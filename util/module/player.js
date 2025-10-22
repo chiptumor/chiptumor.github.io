c8bfdbbcc014ac33b6ca57393c0f02af3ae5692b
chiptumor <141371172+chiptumor@users.noreply.github.com>
2025-10-22 14:20:49 -0500

    begin player.js

--- util/module/player.js
+++ util/module/player.js
@@ -1,3 +1,13 @@
 export async function addPlayer(options) {
-	console.info("ran addPlayer");
+	if (options.menubar) {
+		window.addEventListener("load", async () => {
+			const element = document.getElementById("menubar");
+			if (!element) throw new Error("No menubar element found.");
+
+			const xml = await fetch("/util/resource/player.xml")
+				.then(response => response.text())
+				.then(text => new DOMParser().parseFromString(text, "text/xml"));
+			
+		});
+	}
 };
