e2ae69f5402d6c381b059f31adb07cce6b3df7da
chiptumor <141371172+chiptumor@users.noreply.github.com>
2025-10-22 13:08:00 -0500

    rewrite menubar

--- util/module/menubar.js
+++ util/module/menubar.js
@@ -1,52 +1,38 @@
 function cycle(nodes, parent) {
 	for (const node of nodes) {
+		const element = document.createElement("li");
+		
+		element.className = node.className;
+		node.getAttribute("title") &&
+			element.setAttribute("data-title", node.getAttribute("title") || "");
+		node.getAttribute("desc")  &&
+			element.setAttribute("data-desc",  node.getAttribute("desc")  || "");
+
 		switch (node.tagName) {
 			case "text": {
-				const element = document.createElement("div");
-
-				element.className = node.className;
-				element.classList.add("item", "text");
 				element.innerHTML = "<span>" + node.innerHTML + "</span>";
-
-				element.setAttribute("data-title", node.getAttribute("title") || "");
-				element.setAttribute("data-desc",  node.getAttribute("desc")  || "");
-				parent.append(element);
 			} break;
 			case "url": {
-				const element = document.createElement("a");
-				
-				element.className = node.className;
-				element.classList.add("item", "url");
-				element.innerHTML = "<span>" + node.innerHTML + "</span>";
 				if (
 					new URL(node.getAttribute("href"), window.location.origin).pathname
-						=== window.location.pathname
+					=== window.location.pathname
 				) element.classList.add("located");
-				if (URL.parse(node.getAttribute("href"))) element.setAttribute("target", "_blank");
 
-				element.setAttribute("href",       node.getAttribute("href")  || "");
-				element.setAttribute("data-title", node.getAttribute("title") || "");
-				element.setAttribute("data-desc",  node.getAttribute("desc")  || "");
-				parent.append(element);
+				const anchor = document.createElement("a");
+				
+				if (URL.parse(node.getAttribute("href"))) anchor.setAttribute("target", "_blank");
+				anchor.innerHTML = "<span>" + node.innerHTML + "</span>";
+
+				anchor.setAttribute("href", node.getAttribute("href") || "");
+
+				element.append(anchor);
 			} break;
 			case "copy": {
-				const element = document.createElement("div");
-				
-				element.className = node.className;
-				element.classList.add("item", "copy");
 				element.innerHTML = "<span>" + node.innerHTML + "</span>";
 				
 				element.setAttribute("data-copy",  node.getAttribute("value") || "");
-				element.setAttribute("data-title", node.getAttribute("title") || "");
-				element.setAttribute("data-desc",  node.getAttribute("desc")  || "");
-
-				parent.append(element);
 			} break;
 			case "dropdown": {
-				const element = document.createElement("div");
-
-				element.className = node.className;
-				element.classList.add("dropdown");
 				if (
 					new URL(node.getAttribute("href"), window.location.origin).pathname
 						=== window.location.pathname
@@ -57,19 +43,20 @@ function cycle(nodes, parent) {
 					const span = document.createElement("span");
 					span.innerHTML = text.nodeValue.trim();
 					element.append(span);
-				}
+				} else console.warn("addMenubar: Dropdown tag doesn't have beginning text node.");
 
-				const child = document.createElement("div");
+				const child = document.createElement("menu");
 				child.classList.add("content");
 				element.append(child);
 
 				cycle(node.children, child);
-				parent.append(element);
 			} break;
 			default: {
-				console.warn("addMenubar: Unrecognized tag name!", node);
+				console.warn("addMenubar: Unrecognized tag name.", node);
 			}
 		}
+
+		parent.append(element);
 	}
 }
 
@@ -80,16 +67,15 @@ export async function addMenubar() {
 		const xml = await fetch("/util/resource/menubar.xml")
 			.then(response => response.text())
 			.then(text => new DOMParser().parseFromString(text, "text/xml"));
-		const menubar = document.createElement("div");
-		menubar.setAttribute("id", "menubar");
+		const div = document.createElement("div");
+		div.setAttribute("id", "menubar");
 
-		const element = document.createElement("div");
-		element.classList.add("content");
-		menubar.append(element);
+		const menu = document.createElement("menu");
+		div.append(menu);
 		
-		cycle(xml.documentElement.children, element);
+		cycle(xml.documentElement.children, menu);
 
-		document.body.prepend(menubar);
+		document.body.prepend(div);
 	});
 	
 	console.timeEnd("Create Menubar");
