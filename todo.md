2172aa2d85f0071dda9880074e7df854982062a8
Hazel <141371172+chiptumor@users.noreply.github.com>
2025-12-02 15:33:43 -0600

    generic save

---
+++ todo.md
@@ -0,0 +1,1 @@
+- this is a bullet point
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
+import { JSDOM } from "jsdom";
 
 export default defineConfig({
+    plugins: [
+        {
+            name: "",
+            async transformIndexHtml(html) {
+                const document = new JSDOM(html);
+            }
+        }
+    ],
     build: {
         rollupOptions: {
             input: {
