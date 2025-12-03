7ddedbce10169ee4357b5b0d1bbb09443f26d8ea
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2025-12-02 22:00:14 -0600

    generic save

--- src/main.js
+++ src/main.js
@@ -1,28 +1,6 @@
 import "./util/module/lucide.js";
 
-//#region banner
-addEventListener("load", async () => {
-    
-});
-
 //#region greeting
 addEventListener("load", async () => {
     
-});
-
-//#region status
-addEventListener("load", async () => {
-    
-});
-
-//#region todo list
-addEventListener("load", async () => {
-    const url = "https://api.github.com/repos/chiptumor/chiptumor.github.io/contents/todo.md";
-    const headers = {
-        "Accept": "application/vnd.github.html+json"
-        // https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#get-repository-content
-    };
-    
-    const text = await fetch(url, { headers })
-        .then(response => response.text());
 });
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,62 @@
 import { defineConfig } from "vite";
-import { JSDOM } from "jsdom";
+import { glob } from "glob";
+import * as Path from "path";
+import * as FileSystem from "node:fs/promises";
+
+import { Octokit } from "octokit";
+import { XMLParser } from "fast-xml-parser";
+
+const octokit = new Octokit();
+
+const handlebar = {
+    opengraph: {
+        pfp:
+            await octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
+                owner: "chiptumor",
+                repo: "chiptumor",
+                path: "main/res/profile/rest.json",
+                headers: {
+                    "X-GitHub-Api-Version": "2022-11-28",
+                    "accept": "application/vnd.github.raw+json"
+                }
+            })
+                .then(response => response.json())
+                .then(({ generic: { icon: { zoologist: array }}}) =>
+                    array[Math.floor(Math.random() * array.length)].path
+                )
+    },
+    content: {
+        marquee: await (async () => {
+            
+        })()
+    }
+};
 
 export default defineConfig({
     plugins: [
         {
-            name: "",
+            name: "replace-handlebars",
             async transformIndexHtml(html) {
-                const document = new JSDOM(html);
+                const regex = /\{\{\s*([\w.]+)\s*\}\}/g;
+                return html.replace(regex, (match, path) => {
+                    const value = path.split(".").reduce((obj, key) => obj?.[key], handlebar);
+                    return value ?? match;
+                });
+
+                const response = octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
+                    owner: "chiptumor",
+                    repo: "chiptumor.github.io",
+                    path: "todo.md",
+                    headers: {
+                        "X-GitHub-Api-Version": "2022-11-28"
+                    }
+                })
             }
         }
     ],
     build: {
         rollupOptions: {
-            input: {
-                main: "src/index.html"
-            }
+            input: glob.sync("src/**/*.html")
         }
     }
 })
