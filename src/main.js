3a1e331419b06717edb33411edd3418ab3652c81
Hazel <141371172+chiptumor@users.noreply.github.com>
2025-12-08 12:50:16 -0600

    vibe save

--- src/main.js
+++ src/main.js
@@ -1,6 +1,1 @@
 import TimeAgo from "https://esm.sh/javascript-time-ago@2.5.12";
-
-//#region greeting
-addEventListener("load", async () => {
-    
-});
--- vite.config.js
+++ vite.config.js
@@ -3,51 +3,66 @@ import { glob } from "glob";
 import * as FileSystem from "node:fs/promises";
 
 import { Octokit } from "octokit";
-import { DOMParser, XMLSerializer } from "@xmldom/xmldom";
+import { JSDOM } from "jsdom";
 
 const octokit = new Octokit();
+async function github({ repo, path }) {
+    return await octokit.request("GET /repos/{repo}/contents/{path}", {
+        repo, path,
+        headers: {
+            "x-GitHub-Api-Version": "2022-11-28",
+            "accept": "application/vnd.github.raw+json"
+        }
+    });
+}
+
+function parseDOM(string) {
+    return new JSDOM(string, { contentType: "text/xml" });
+}
 
 const handlebar = {
     content: {
         opengraph: {
             avatar: await (async () => {
                 const { generic: { icon: { zoologist: array }}} =
-                    await octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
-                        owner: "chiptumor",
-                        repo: "chiptumor",
-                        path: "main/res/profile/rest.json",
-                        headers: {
-                            "X-GitHub-Api-Version": "2022-11-28",
-                            "accept": "application/vnd.github.raw+json"
-                        }
+                    await github({
+                        repo: "chiptumor/chiptumor",
+                        path: "main/res/profile/rest.json"
                     })
                     .then(response => response.json());
                 return array[Math.floor(Math.random() * array.length)].path;
             })()
         },
         menubar: await (async () => {
-            const element = await FileSystem.readFile("./lib/menubar.xml", "utf8");
+            const element = await FileSystem.readFile("./lib/menubar.inc", "utf8");
             return element;
         })(),
         marquee: await (async () => {
             const xml =
                 await FileSystem.readFile("./content/marquee.xml", "utf8")
-                .then(file => new DOMParser().parseFromString(file, "text/xml"));
+                .then(file => parseDOM(file));
             return {
                 visible: xml.documentElement.getAttribute("visible"),
-                summary: xml.getElementsByTagName("preview")[0].nodeValue,
-                details: xml.getElementsByTagName("body")[0].nodeValue
+                summary: xml.getElementsByTagName("preview")[0].innerHTML,
+                details: xml.getElementsByTagName("body")[0].innerHTML
             };
         })(),
         status: await (async () => {
             const xml =
                 await FileSystem.readFile("./content/status.xml", "utf8")
-                .then(file => new DOMParser().parseFromString(file, "text/xml"));
+                .then(file => parseDOM(file));
             return {
-                feeling: xml.getElementsByTagName("imFeeling")[0].nodeValue,
-                body: xml.getElementsByTagName("body")[0].nodeValue
+                feeling: xml.getElementsByTagName("imFeeling")[0].innerHTML,
+                body: xml.getElementsByTagName("body")[0].innerHTML
             };
         })(),
+        social: {
+            discord: {
+                profile: await (async () => {
+
+                })()
+            }
+        },
         todo: await (async () => {
 
         })()
