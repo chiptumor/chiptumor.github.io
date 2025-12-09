37fd5334a76f9bc97cb864729c90ec938c7ae492
Hazel <141371172+chiptumor@users.noreply.github.com>
2025-12-09 14:00:28 -0600

    use replace.js

--- src/util/script/replace.js
+++ src/util/script/replace.js
@@ -1,5 +1,23 @@
 export default function (object) {
     document.addEventListener("DOMContentLoaded", () => {
-        
+        document.querySelectorAll("replace").forEach(element => {
+            const regex = /\s*([\w.]+)\s*/;
+            const item =
+                element.textContent.replace(regex, (_, path) =>
+                    path.split(".").reduce((obj, key) => obj?.[key], object)
+                );
+            element.replaceWith(item ?? element);
+        });
+
+        document.querySelectorAll("[data-replace-attr]").forEach(element => {
+            const regex = /\s+/g;
+            element.getAttribute("data-replace-attr").split(regex).forEach(attr => {
+                const item =
+                    element.getAttribute(attr)
+                    .split(".").reduce((obj, key) => obj?.[key], object);
+                element.setAttribute(attr, item ?? "undefined");
+            });
+            element.removeAttribute("data-replace-attr");
+        });
     });
 }
