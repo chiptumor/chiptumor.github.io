e282d5c832a45c7d0410ff56b0de9ae546fd1aa7
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2025-12-09 15:48:46 -0600

    fix timeago mod

--- src/util/module/timeago.js
+++ src/util/module/timeago.js
@@ -1,6 +1,7 @@
 import TimeAgo from "https://esm.sh/javascript-time-ago";
 
 await fetch("https://esm.sh/javascript-time-ago/locale/en.json")
+.then(response => response.json())
 .then(TimeAgo.addDefaultLocale);
 
 export default new TimeAgo("en-US");
