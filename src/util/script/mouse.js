a2045084b531c08067dba850734881cc2cbce8a2
Hazel <141371172+chiptumor@users.noreply.github.com>
2025-12-15 14:04:58 -0600

    feat: add mouse utils

---
+++ src/util/script/mouse.js
@@ -0,0 +1,46 @@
+const element = document.createElement("div");
+    element.setAttribute("id", "tooltip");
+
+const titleEl = document.createElement("span");
+    titleEl.classList.add("title");
+const descEl = document.createElement("span");
+    descEl.classList.add("desc");
+const clipboard = document.createElement("span");
+    clipboard.classList.add("clip");
+
+const clipSuccess = document.createElement("span");
+    clipSuccess.classList.add("success");
+    clipSuccess.innerHTML = "Copied!";
+
+clipboard.append(clipSuccess);
+element.append(titleEl, descEl, clipboard);
+document.prepend(element);
+
+window.addEventListener("mousemove", event => {
+    element.style.top = (event.clientY + 20) + "px";
+    element.style.left = (event.clientX) + "px";
+    
+    const hovered = event.target.closest("[data-title], [data-desc]");
+    const title = hovered?.getAttribute("data-title");
+    const desc = hovered?.getAttribute("data-desc");
+
+    if (title || desc) {
+        element.style.display = "flex";
+        titleEl.innerHTML = title;
+        descEl.innerHTML = desc;
+    } else {
+        element.style.display = "none";
+        titleEl.innerHTML = "";
+        descEl.innerHTML = "";
+    }
+});
+
+window.addEventListener("click", event => {
+    const clip = event.target.closest("[data-copy]");
+    if (clip) {
+        navigator.clipboard.writeText(clip.getAttribute("data-copy"));
+        element.classList.add("copied");
+    }
+
+    setTimeout(() => element.classList.remove("copied"), 1500);
+});
